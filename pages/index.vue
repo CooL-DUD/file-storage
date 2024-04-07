c2a730d81c8edff7a3a716fbc43a1f98ce81feff
gani <gani.nurgazy.03@gmail.com>
2024-04-07 22:01:49 +0500

    feat: upload file, get file and download it

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,131 @@
 <script setup lang="ts">
+import GUN from 'gun';
+import 'gun/sea';
+import 'gun/axe';
 
+const db = GUN()
+const user = db.user()
+const router = useRouter()
+const { $toast, $toastError } = useNuxtApp()
+
+const userData = ref({
+  username: '',
+  password: ''
+})
+
+const form = ref({
+  type: 'login'
+})
+
+function registerUser() {
+  user.create(userData.value.username, userData.value.password, (ack) => {
+    console.log('ack',ack)
+    console.log(userData.value)
+    if (ack.ok) {
+      $toast('User created successfully! Redirecting to profile page...')
+      setTimeout(() => {
+        router.push('/profile')
+      }, 3000)
+    } else if (ack.err) {
+      $toastError(ack.err)
+    }
+  })
+}
+
+function loginUser() {
+  user.auth(userData.value.username, userData.value.password, (ack) => {
+    console.log(ack)
+    if (ack.err) {
+      $toastError(ack.err)
+    } else {
+      $toast('User logged in successfully! Redirecting to profile page...')
+      setTimeout(() => {
+        router.push('/profile')
+      }, 3000)
+    }
+  })
+}
 </script>
 
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/auth">Auth</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/profile">Profile</nuxt-link>
-        </li>
-      </ul>
-    </nav>
+  <div v-if="form.type === 'login'" class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form @submit.prevent="loginUser" class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input v-model="userData.username" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input v-model="userData.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Don't have an account?
+        {{ ' ' }}
+        <p @click="form.type = 'register'" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create account</p>
+      </p>
+    </div>
+  </div>
+
+  <div v-else-if="form.type === 'register'" class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create account</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form @submit.prevent="registerUser" class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input v-model="userData.username" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input v-model="userData.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Already have account?
+        {{ ' ' }}
+        <p @click="form.type = 'login'" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign in</p>
+      </p>
+    </div>
   </div>
 </template>
 
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,9 +1,74 @@
 <script setup>
+import GUN from 'gun';
+const db = GUN();
+
+const uploadFileElement = ref(null)
+
+const downloadUrl = ref({
+  url: '',
+  file_name: ''
+})
+async function uploadFile() {
+  const data = {
+    file_base64: await convertFileToBase64(uploadFileElement.value.files[0]),
+    file_name: uploadFileElement.value.files[0].name,
+    size: uploadFileElement.value.files[0].size,
+    type: uploadFileElement.value.files[0].type,
+  }
+  db.get('files').get('alias').put(data, (ack) => {
+    console.log('put file', ack)
+  })
+}
+
+function convertFileToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64String = reader.result.split(',')[1]; // Remove data URL prefix
+      resolve(base64String);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
+
+function getFiles() {
+  console.log(db.get('alias').get('files').map())
+  // db.get('alias').get('files', (ack) => {
+  //   const blob = base64toBlob(ack.put.file_base64)
+  //   downloadUrl.value = {
+  //     url: URL.createObjectURL(blob),
+  //     file_name: ack.put.file_name,
+  //   }
+  // })
+}
+
+function base64toBlob(base64Data) {
+  const byteCharacters = atob(base64Data);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray]);
+}
 </script>
 
 <template>
   <div>
-    profile
+    <form @submit.prevent="uploadFile">
+      <input type="file" ref="uploadFileElement"/>
+      <button>upload</button>
+    </form>
+
+    <button @click="getFiles">get file</button>
+
+    <a v-if="downloadUrl.url" :href="downloadUrl.url" :download="downloadUrl.file_name">download</a>
   </div>
 </template>
 
