b4ed7f2024afd9a2c501db8cf01c3a07dae8a5dc
gani <gani.nurgazy.03@gmail.com>
2024-05-29 22:15:40 +0500

    feat: env

--- server/utils/token.ts
+++ server/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import {TOKEN_SECRET} from "~/server/env";
+// import {TOKEN_SECRET} from "~/server/env";
 
 interface TokenPayload {
     user_id: string,
@@ -8,14 +8,14 @@ interface TokenPayload {
 }
 
 export function generateToken(payload: TokenPayload) {
-    return jwt.sign(payload, TOKEN_SECRET, { expiresIn: '1d' })
+    return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '1d' })
 }
 export function generateRefresh(payload: TokenPayload) {
-    return jwt.sign(payload, TOKEN_SECRET, { expiresIn: '30d' })
+    return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '30d' })
 }
 
 export function verifyToken(token: TokenPayload) {
-    return jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+    return jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
         if (err) {
             console.error('Error verifying token:', err);
             return {error: true, data: err};
