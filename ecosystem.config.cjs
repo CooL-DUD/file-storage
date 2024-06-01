35c59f4c5ec771f76f8c9e168506d8a3cc9fc95c
gani <gani.nurgazy.03@gmail.com>
2024-06-01 19:09:13 +0500

    fixes for deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,17 +7,5 @@ module.exports = {
             instances: 'max',
             script: './.output/server/index.mjs'
         }
-    ],
-    deploy : {
-        // "production" is the environment name
-        production : {
-            user: 'root',
-            host: ['207.154.229.180'],
-            ref: 'origin/master',
-            repo: 'git@github.com:CooL-DUD/file-storage.git',
-            ssh_options: ['ForwardAgent=yes'],
-            path: '/var/www/file-sandyq',
-            'post-deploy' : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production'
-        }
-    }
+    ]
 }
